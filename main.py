e6ab293bb3be1dab4b1609fcb8d1f923f43d6f79
martin <martin-cleven@gmx.net>
2021-04-13 20:12:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from unityagents import UnityEnvironment
+import numpy as np
+
+# please do not modify the line below
+# env = UnityEnvironment(file_name="/data/Banana_Linux_NoVis/Banana.x86_64")
+env = UnityEnvironment('data/Banana_Linux/Banana.x86_64')
